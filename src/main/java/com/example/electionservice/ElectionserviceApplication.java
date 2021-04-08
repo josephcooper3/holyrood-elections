ff2f578b50115bed19533ee8d06fb603020eebe8
Joe Cooper <52382338+josephcooper3@users.noreply.github.com>
2021-04-08 20:49:51 +0100

    [Initial Commit]

---
+++ src/main/java/com/example/electionservice/ElectionserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.electionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectionserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectionserviceApplication.class, args);
+	}
+
+}
