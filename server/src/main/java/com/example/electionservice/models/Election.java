346f9d42ffc1db05a73e276615a77ece91c3f099
Joe Cooper <52382338+josephcooper3@users.noreply.github.com>
2021-04-13 21:32:13 +0100

    [Add] JPA annotations to Election class

--- server/src/main/java/com/example/electionservice/models/Election.java
+++ server/src/main/java/com/example/electionservice/models/Election.java
@@ -4,13 +4,23 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 
+@Entity
+@Table(name = "elections")
 @Getter @Setter @NoArgsConstructor
 public class Election {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "year")
     private int year;
+
+    @Column(name = "seats")
     private ArrayList<String> seats;
 
     public Election(int year, ArrayList<String> seats) {
