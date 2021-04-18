f1674e0a6836a54848015b417baa116653a8273e
Joe Cooper <52382338+josephcooper3@users.noreply.github.com>
2021-04-18 21:59:40 +0100

    [Remove] custom route

--- server/src/main/java/com/example/electionservice/controllers/ElectionController.java
+++ server/src/main/java/com/example/electionservice/controllers/ElectionController.java
@@ -1,31 +1,10 @@
 package com.example.electionservice.controllers;
 
-import com.example.electionservice.models.Election;
-import com.example.electionservice.repositories.ElectionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/elections")
 public class ElectionController {
-
-    @Autowired
-    ElectionRepository electionRepository;
-
-    @GetMapping
-    public List<Election> getAllElections() {
-        return electionRepository.findAll();
-    }
-
-    @GetMapping("{id}")
-    public Optional<Election> getElection(@PathVariable Long id) {
-        return electionRepository.findById(id);
-    }
 
 }
--- server/src/main/java/com/example/electionservice/controllers/SeatController.java
+++ server/src/main/java/com/example/electionservice/controllers/SeatController.java
@@ -1,31 +1,10 @@
 package com.example.electionservice.controllers;
 
-import com.example.electionservice.models.Seat;
-import com.example.electionservice.repositories.SeatRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/seats")
 public class SeatController {
-
-    @Autowired
-    SeatRepository seatRepository;
-
-    @GetMapping
-    public List<Seat> getAllSeats() {
-        return seatRepository.findAll();
-    }
-
-    @GetMapping("{id}")
-    public Optional<Seat> getElection(@PathVariable Long id) {
-        return seatRepository.findById(id);
-    }
 
 }
