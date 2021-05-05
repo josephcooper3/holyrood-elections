7d5ce4f441b82b22850c8fcca3f883df6bd54aa3
Joe Cooper <52382338+josephcooper3@users.noreply.github.com>
2021-05-05 22:53:19 +0100

    [Add] full Edinburgh Central 2016 results to Seeds

--- server/src/main/java/com/example/electionservice/components/DataLoader.java
+++ server/src/main/java/com/example/electionservice/components/DataLoader.java
@@ -1,8 +1,12 @@
 package com.example.electionservice.components;
 
 import com.example.electionservice.models.Election;
+import com.example.electionservice.models.Party;
+import com.example.electionservice.models.Result;
 import com.example.electionservice.models.Seat;
 import com.example.electionservice.repositories.ElectionRepository;
+import com.example.electionservice.repositories.PartyRepository;
+import com.example.electionservice.repositories.ResultRepository;
 import com.example.electionservice.repositories.SeatRepository;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,13 +24,44 @@ public class DataLoader implements ApplicationRunner {
     @Autowired
     SeatRepository seatRepository;
 
+    @Autowired
+    PartyRepository partyRepository;
+
+    @Autowired
+    ResultRepository resultRepository;
+
     public void run(ApplicationArguments args) {
+
+        Party conservative = new Party("Scottish Conservatives");
+        partyRepository.save(conservative);
+
+        Party snp = new Party("Scottish National Party");
+        partyRepository.save(snp);
+
+        Party labour = new Party("Scottish Labour");
+        partyRepository.save(labour);
+
+        Party green = new Party("Scottish Green Party");
+        partyRepository.save(green);
+
+        Party libDem = new Party("Liberal Democrats");
+        partyRepository.save(libDem);
+
+        Party libertarian = new Party("Scottish Libertarian Party");
+        partyRepository.save(libertarian);
 
         Election sixteen = new Election(2016);
         electionRepository.save(sixteen);
 
         Seat edinCentral = new Seat("Edinburgh Central");
         seatRepository.save(edinCentral);
+
+        resultRepository.save(new Result(sixteen, edinCentral, conservative,  10399));
+        resultRepository.save(new Result(sixteen, edinCentral, snp, 9789));
+        resultRepository.save(new Result(sixteen,edinCentral, labour, 7546));
+        resultRepository.save(new Result(sixteen, edinCentral, green, 4644));
+        resultRepository.save(new Result(sixteen, edinCentral, libDem, 1672));
+        resultRepository.save(new Result(sixteen, edinCentral, libertarian, 119));
 
     }
 
